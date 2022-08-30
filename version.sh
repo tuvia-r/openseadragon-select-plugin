3c10954b93272fcbebf4dc354569e2a55d26909c
Tuvia Rumpler <tuvia.r@uveye.com>
2022-08-31 02:43:03 +0300

    add auto update for demo version

--- version.sh
+++ version.sh
@@ -2,4 +2,8 @@
 
 npx auto-changelog -p
 
+version=$(node -p -e "require('./package.json').version")
+
+json -I -f demo/package.json -e "this.dependencies['openseadragon-select-plugin']=\"$version\""
+
 git add .
