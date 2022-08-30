03e3b48220516d930bf69f68dfddcd5e65a67a24
Tuvia Rumpler <tuvia.r@uveye.com>
2022-08-31 01:59:49 +0300

    add option to keep shape

--- src/selection.ts
+++ src/selection.ts
@@ -9,15 +9,21 @@ export type ViewerSelectionType = (options: {
 
 export function selection(
 	this: Viewer,
-	options: { onSelection: (rect: Rect, shape: BaseShape) => void, keep: boolean },
+	options: {
+		onSelection: (rect: Rect, shape: BaseShape) => void;
+		keep: boolean;
+	},
 ) {
 	this.initSelection();
-	if(options.keep) {
+	if (options.keep) {
 		const originalCallback = options.onSelection;
-		options.onSelection = (rect: Rect, shape: BaseShape) => {
-			this.selectionHandler.addShape(shape)
+		options.onSelection = (
+			rect: Rect,
+			shape: BaseShape,
+		) => {
+			this.selectionHandler.addShape(shape);
 			return originalCallback(rect, shape);
-		}
+		};
 	}
 	return new ShapeSelection(
 		this.selectionHandler.frontCanvas,
