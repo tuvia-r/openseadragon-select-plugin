8a3d9d0cfa17eb302d69ce47814d2276ba2bcfb7
IdanCo <idan@idancohen.com>
2022-08-31 01:24:33 +0300

    Demo improvements

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,16 +1,41 @@
 import * as OpenSeadragon from 'openseadragon';
 import 'openseadragon-select-plugin';
 
-function callBack(rect, shape) {
-    console.info(rect, shape);
-    document.getElementById('selection').innerText = JSON.stringify(rect);
-}
-
 const viewer = OpenSeadragon({
     id: "app",
-    tileSources: 'https://openseadragon.github.io/example-images/highsmith/highsmith.dzi'
+    tileSources: 'https://openseadragon.github.io/example-images/highsmith/highsmith.dzi',
+    showNavigationControl: false, // zoom, etc.
+    homeFillsViewer: true, // looks better in artemis
+    visibilityRatio: 1, // prevent moving image outside viewport
 });
-const selection = viewer.selection({ onSelection: callBack });
-selection.enable();
 
-console.info(123);
+const selection = viewer.selection({ onSelection: onSelect });
+
+let isSelecting = false;
+
+function toggleSelection() {
+    isSelecting = !isSelecting;
+    if (isSelecting) {
+        selection.enable();
+        btn.innerText = 'Disable Selection'
+        btn.classList.toggle('btn-danger')
+    } else {
+        selection.disable();
+        btn.innerText = "Enable Selection"
+        btn.classList.toggle('btn-danger')
+    }
+}
+
+const btn = document.getElementById('toggle-button');
+btn.addEventListener("click", function () {
+    toggleSelection();
+});
+
+function onSelect(rect, shape) {
+    console.info(rect, shape);
+    document.getElementById('selection').innerText = `x: ${Math.round(rect.x)} | y: ${Math.round(rect.y)} | width: ${Math.round(rect.width)} | height: ${Math.round(rect.height)}`
+    isSelecting = false;
+    btn.innerText = "Enable Selection"
+    btn.classList.toggle('btn-danger')
+}
+
