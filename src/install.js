3106e0fe7a07175503da770a4a3c6e39469c1f40
Tuvia Rumpler <tuvia.r@uveye.com>
2022-09-16 16:03:26 +0300

    fix lint

--- src/install.js
+++ src/install.js
@@ -1,12 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 (function () {
-    var osd = window.OpenSeadragon;
-    var plugin = require('./selection');
-    if (!osd) {
-        osd = require('openseadragon');
-        if (!osd) {
-            throw new Error('OpenSeadragon is missing.');
-        }
-    }
-    osd.Viewer.prototype.selection = plugin.selection;
-    osd.Viewer.prototype.initSelection = plugin.initSelection;
-})()
+	var osd = window.OpenSeadragon;
+	var plugin = require('./selection');
+	if (!osd) {
+		osd = require('openseadragon');
+		if (!osd) {
+			throw new Error('OpenSeadragon is missing.');
+		}
+	}
+	osd.Viewer.prototype.selection = plugin.selection;
+	osd.Viewer.prototype.initSelection =
+		plugin.initSelection;
+})();
